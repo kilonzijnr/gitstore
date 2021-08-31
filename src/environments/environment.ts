16937444e328fa298b501a852eef4c5a15a4f241
kilonzijnr <vkilonzi05@gmail.com>
2021-08-31 11:57:51 +0300

    added api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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'
 };
 
 /*
