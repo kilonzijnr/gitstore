7d6e9931bc257e3c0af7d3716c3673fda478a828
kilonzijnr <vkilonzi05@gmail.com>
2021-09-01 10:09:38 +0300

    genarated a repo class

--- src/app/searchservice/search.service.ts
+++ src/app/searchservice/search.service.ts
@@ -3,6 +3,8 @@ import { User} '../user'
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import { Repository } from '../repository';
+import { UserrepoComponent } from '../userrepo/userrepo.component';
+import { resolve } from 'dns';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +13,24 @@ private formRepo:string = ''
 user!: User;
 repository!:Repository[];
 private userName ='';
+userrepo:any;
   constructor(private http: HttpClient) { 
     this.user = new User ("", "",0, 0, 0, "");
   }
+userRequest(userName:string){
+  this.userName=userName
+  console.log(this.userName);
+    
+  interface ApiResponse {
+    name:string;
+    avatar_url: string;
+    followers: number;
+    following: number;
+    public_repos: number;
+    bio: string;
+  }
+  let promise = new Promise((resolve, reject)) => {
+    this.http.get<ApiResponse>('')
+  }
+}
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,10 @@
 export class User {
+  constructor(
+    public photo: string,
+    public userName: string,
+    public followers: number,
+    public following: number,
+    public repos: number,
+    public bio: string
+  ) {}
 }
