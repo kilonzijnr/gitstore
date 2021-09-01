e7f10519db794426e073460fd61adae845df7b8c
kilonzijnr <vkilonzi05@gmail.com>
2021-09-01 14:04:17 +0300

    linked api links to enviroment and added heading to user compenent

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  userApi: 'https://api.github.com/users',
+  repoApi: '"https://api.github.com/search/repositories?q=',
+  apiKey: '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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiKey:'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'
+  userApi: 'https://api.github.com/users/',
+  repoApi: 'https://api.github.com/search/repositories?q=',
+  apiKey: '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'
 };
 
 /*
